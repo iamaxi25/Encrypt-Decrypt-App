c02c25f33d6836a3fc8b67faa2f84c2978a8ced2
iamaxi25 <alexandru.paraschivoiu25@gmail.com>
2023-02-28 21:06:55 +0200

    Added decrypt function

---
+++ script.js
@@ -0,0 +1,24 @@
+const largeWheel = ["A", "B", "C", "D", "E", "F", "G", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "X", "Z", "1", "2", "3", "4"];
+const smallWheel = ["g", "k", "l", "n", "p", "r", "t", "u", "z", "&", "x", "y", "s", "o", "m", "q", "i", "h", "f", "d", "b", "a", "c", "e"];
+
+
+function decrypt(cipherText) {
+    let plainText = "";
+    for (let i = 0; i < cipherText.length; i++) {
+      const index = smallWheel.indexOf(cipherText[i].toLowerCase());
+      if (index !== -1) {
+        plainText += largeWheel[index];
+      } else {
+        plainText += cipherText[i];
+      }
+    }
+    return plainText;
+  }
+  
+
+  const decryptButton = document.querySelector(".decrypt");
+decryptButton.addEventListener("click", function() {
+  const cipherText = document.querySelector("#hashes").value;
+  const plainText = decrypt(cipherText);
+  document.querySelector("#hashes").value = plainText;
+});
